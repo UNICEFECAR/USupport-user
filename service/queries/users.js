2f496cc0d295f7efcda9ad1293c9dc83a69928d7
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-13 03:29:01 +0000

    Refactor: use field deleted_at instead of is_deleted

--- service/queries/users.js
+++ service/queries/users.js
@@ -60,10 +60,10 @@ export const getProviderUserByEmail = async (poolCountry, email) =>
 export const getUserByID = async (poolCountry, user_id) =>
   await getDBPool("piiDb", poolCountry).query(
     `
-        SELECT user_id, country_id, type, client_detail_id, notification_preference_id, password, is_deleted
+        SELECT user_id, country_id, type, client_detail_id, notification_preference_id, password
         FROM "user"
         WHERE user_id = $1
-        AND is_deleted = false
+        AND deleted_at IS NULL
         ORDER BY created_at DESC
         LIMIT 1;
         
