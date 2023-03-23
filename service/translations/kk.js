5ef1a9c075543bf5a074317d50ae4f8c3b3c0ab0
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-03-24 00:52:09 +0200

    Add: russian and kazakh translations

---
+++ service/translations/kk.js
@@ -0,0 +1,23 @@
+export default {
+  email_or_access_token_required_error:
+    "Тіркелу үшін Сізге электронды поштаңызды немесе пайдаланушының жарамды кіру токенін көрсету қажет.",
+  nickname_required_error: "Сізге лақап атыңызды көрсету қажет",
+  password_validation_error:
+    "Кілтсөз кемінде 8 таңбадан, 1 бас әріптен, 1 кіші әріптен және 1 саннан тұруы керек.",
+  user_not_found_error: "Пайдаланушы табылмады",
+  notification_preferences_not_found_error:
+    "Хабарландыру баптаулары табылмады",
+  country_not_found_error: "Ел табылмады",
+  email_already_used_error: "Бұл электронды пошта әлдеқашан қолданылып жатыр",
+  user_access_token_already_used_error:
+    "Бұл пайдаланушының кіру токені қолжетімді емес",
+  incorrect_email_error: "Осындай электронды поштасы бар пайдаланушы жоқ",
+  incorrect_password_error: "Енгізілген кілтсөз дұрыс емес",
+  not_authenticated_error: "Бұл ресурсқа кіру үшін пайдаланушы аутентификациядан өтуі керек",
+  invalid_refresh_token_error: "Жаңарту токені жарамсыз немесе әлдеқашан қолданылып жатыр",
+  cannot_generate_user_access_token_error: `Жаңа кездейсоқ пайдаланушының кіру токенін жасау мүмкін болмады. Тағы бір көріңіз`,
+  invalid_reset_password_token_error: "Кілтсөзді өзгерту токені жарамсыз немесе мерзімі өтіп кеткен",
+  invalid_provider_otp_error: "Invalid OTP",
+  too_many_otp_requests_error:
+    "Too many OTP requests made, please try again soon.",
+};
