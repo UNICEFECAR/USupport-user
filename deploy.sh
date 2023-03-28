7007e0c5ae191fbdce0d40e8e541c82c9eb7c017
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2023-03-29 00:53:35 +0100

    refactor deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,25 @@
-# Build docker image of the service locally
-docker build -t user:latest .
+REDEPLOY=$1
 
-docker tag user:latest 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-user-api
+if [ "$REDEPLOY" = 'deploy' ] || [ "$REDEPLOY" = 'redeploy' ]
+then
+    # Build docker image of the service locally
+    docker build -t user:latest .
 
-# Push image to 
-docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-user-api
+    docker tag user:latest 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-user-api
 
-# Update Kuberenetes Cluster applications for this API service
-kubectl apply -f config.yaml -f secrets.yaml -f deployment.yaml -f service.yaml
+    # Push image to 
+    docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-user-api
+
+    if [ "$REDEPLOY" = 'deploy' ]
+    then
+        # Update Kuberenetes Cluster applications for this API service
+        kubectl apply -f config.yaml -f secrets.yaml -f deployment.yaml -f service.yaml
+    elif [ "$REDEPLOY" = 'redeploy' ]
+    then 
+        kubectl apply -f config.yaml -f secrets.yaml -f service.yaml
+        kubectl rollout restart deployment user
+    fi
+
+else
+    echo "Please select either to deploy or redeploy k8s pod"
+fi
