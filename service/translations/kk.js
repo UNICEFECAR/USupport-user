1ce94a6d0e575101ed3c5c15de4d9f6eac4d7fa4
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-06-05 11:07:30 +0300

    Add: missing translations

--- service/translations/kk.js
+++ service/translations/kk.js
@@ -5,19 +5,21 @@ export default {
   password_validation_error:
     "Кілтсөз кемінде 8 таңбадан, 1 бас әріптен, 1 кіші әріптен және 1 саннан тұруы керек.",
   user_not_found_error: "Пайдаланушы табылмады",
-  notification_preferences_not_found_error:
-    "Хабарландыру баптаулары табылмады",
+  notification_preferences_not_found_error: "Хабарландыру баптаулары табылмады",
   country_not_found_error: "Ел табылмады",
   email_already_used_error: "Бұл электронды пошта әлдеқашан қолданылып жатыр",
   user_access_token_already_used_error:
     "Бұл пайдаланушының кіру токені қолжетімді емес",
   incorrect_email_error: "Осындай электронды поштасы бар пайдаланушы жоқ",
   incorrect_password_error: "Енгізілген кілтсөз дұрыс емес",
-  not_authenticated_error: "Бұл ресурсқа кіру үшін пайдаланушы аутентификациядан өтуі керек",
-  invalid_refresh_token_error: "Жаңарту токені жарамсыз немесе әлдеқашан қолданылып жатыр",
+  not_authenticated_error:
+    "Бұл ресурсқа кіру үшін пайдаланушы аутентификациядан өтуі керек",
+  invalid_refresh_token_error:
+    "Жаңарту токені жарамсыз немесе әлдеқашан қолданылып жатыр",
   cannot_generate_user_access_token_error: `Жаңа кездейсоқ пайдаланушының кіру токенін жасау мүмкін болмады. Тағы бір көріңіз`,
-  invalid_reset_password_token_error: "Кілтсөзді өзгерту токені жарамсыз немесе мерзімі өтіп кеткен",
-  invalid_provider_otp_error: "Invalid OTP",
+  invalid_reset_password_token_error:
+    "Кілтсөзді өзгерту токені жарамсыз немесе мерзімі өтіп кеткен",
+  invalid_provider_otp_error: "Жарамсыз бір реттік құпия сөз (OTP)",
   too_many_otp_requests_error:
-    "Too many OTP requests made, please try again soon.",
+    "Тым көп бір реттік құпия сөз (OTP) сұраулары жасалды, әрекетті кейінірек қайталаңыз.",
 };
--- service/translations/ru.js
+++ service/translations/ru.js
@@ -5,19 +5,21 @@ export default {
   password_validation_error:
     "Пароль должен содержать не менее 8 символов, 1 заглавную букву, 1 строчную букву и 1 цифру.",
   user_not_found_error: "Пользователь не найден",
-  notification_preferences_not_found_error:
-    "Настройки уведомлений не найдены",
+  notification_preferences_not_found_error: "Настройки уведомлений не найдены",
   country_not_found_error: "Страна не найдена",
   email_already_used_error: "Эта электронная почта уже используется",
   user_access_token_already_used_error:
     "Этот токен доступа пользователя недоступен",
   incorrect_email_error: "Нет пользователя с такой электронной почтой",
   incorrect_password_error: "Предоставленный пароль неверен",
-  not_authenticated_error: "Пользователь должен пройти аутентификацию для доступа к этому ресурсу",
-  invalid_refresh_token_error: "Токен обновления недействителен или уже используется",
+  not_authenticated_error:
+    "Пользователь должен пройти аутентификацию для доступа к этому ресурсу",
+  invalid_refresh_token_error:
+    "Токен обновления недействителен или уже используется",
   cannot_generate_user_access_token_error: `Не удалось сгенерировать новый токен доступа случайного пользователя. Пожалуйста, попробуйте еще раз`,
-  invalid_reset_password_token_error: "Недействительный или просроченный токен сброса пароля",
-  invalid_provider_otp_error: "Invalid OTP",
+  invalid_reset_password_token_error:
+    "Недействительный или просроченный токен сброса пароля",
+  invalid_provider_otp_error: "Недействительный одноразовый пароль (OTP)",
   too_many_otp_requests_error:
-    "Too many OTP requests made, please try again soon.",
+    "Сделано слишком много запросов на одноразовый пароль (OTP), повторите попытку позже.",
 };
