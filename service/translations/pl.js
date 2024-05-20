e055932c9b692e26f87968957250a57e969760c5
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2024-05-20 14:42:34 +0300

    Add: polish and ukrainian translations

---
+++ service/translations/pl.js
@@ -0,0 +1,34 @@
+export default {
+  email_or_access_token_required_error:
+    "Aby się zarejestrować, należy podać adres e-mail lub ważny token dostępu użytkownika",
+  nickname_required_error: "Musisz podać nick",
+  password_validation_error:
+    "Hasło powinno zawierać co najmniej 8 znaków, 1 wielką literę, 1 małą literę i 1 cyfrę.",
+  user_not_found_error: "Nie znaleziono użytkownika",
+  notification_preferences_not_found_error:
+    "Nie znaleziono preferencji powiadomień",
+  country_not_found_error: "Nie znaleziono kraju",
+  email_already_used_error: "Ten adres e-mail jest już wykorzystany",
+  user_access_token_already_used_error:
+    "Ten token dostępu użytkownika nie jest dostępny",
+  incorrect_password_error: "Podane hasło jest nieprawidłowe",
+  incorrect_credentials_error: "Nieprawidłowe poświadczenia",
+  not_authenticated_error:
+    "Użytkownik musi się uwierzytelnić, aby uzyskać dostęp do tego zasobu",
+  invalid_refresh_token_error:
+    "Token odświeżania jest nieprawidłowy lub został już wykorzystany",
+  cannot_generate_user_access_token_error:
+    "Nie można wygenerować nowego losowego tokena dostępu użytkownika. Spróbuj ponownie",
+  invalid_reset_password_token_error:
+    "Nieprawidłowy lub wygasły token resetowania hasła. Poproś o nowy pod adresem: ",
+  invalid_provider_otp_error: "Nieprawidłowy OTP",
+  too_many_otp_requests_error:
+    "Wykonano zbyt wiele żądań OTP, spróbuj ponownie wkrótce.",
+  invalid_email_otp_error: "Nieprawidłowy kod weryfikacyjny",
+  email_otp_expired_error: "Kod weryfikacyjny wygasł",
+  invalid_platform_password_error: "Invalid platform password",
+  no_platform_password_set_error: "No platform password is set",
+  password_is_required_error: "Hasło jest wymagane",
+  too_many_login_requests_error:
+    "Too many login requests made, please try again in {1} minutes.",
+};
