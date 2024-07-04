ec86f5b9cef21dc8c59fb3e987c2828d407bf05d
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2024-07-04 18:47:13 +0300

    Refcator: pl translations

--- service/translations/pl.js
+++ service/translations/pl.js
@@ -1,6 +1,6 @@
 export default {
   email_or_access_token_required_error:
-    "Aby się zarejestrować, należy podać adres e-mail lub ważny token dostępu użytkownika",
+    "Aby się zarejestrować, należy podać adres email lub ważny token dostępu użytkownika",
   nickname_required_error: "Musisz podać nick",
   password_validation_error:
     "Hasło powinno zawierać co najmniej 8 znaków, 1 wielką literę, 1 małą literę i 1 cyfrę.",
@@ -8,7 +8,7 @@ export default {
   notification_preferences_not_found_error:
     "Nie znaleziono preferencji powiadomień",
   country_not_found_error: "Nie znaleziono kraju",
-  email_already_used_error: "Ten adres e-mail jest już wykorzystany",
+  email_already_used_error: "Ten adres email jest już wykorzystany",
   user_access_token_already_used_error:
     "Ten token dostępu użytkownika nie jest dostępny",
   incorrect_password_error: "Podane hasło jest nieprawidłowe",
@@ -26,9 +26,10 @@ export default {
     "Wykonano zbyt wiele żądań OTP, spróbuj ponownie wkrótce.",
   invalid_email_otp_error: "Nieprawidłowy kod weryfikacyjny",
   email_otp_expired_error: "Kod weryfikacyjny wygasł",
-  invalid_platform_password_error: "Nieprawidłowe hasło platformy",
-  no_platform_password_set_error: "Hasło platformy nie zostało ustawione",
+  invalid_platform_password_error: "Nieprawidłowe hasło dostępu do platformy",
+  no_platform_password_set_error:
+    "Hasło dostępu do platformy nie zostało jeszcze ustawione",
   password_is_required_error: "Hasło jest wymagane",
   too_many_login_requests_error:
-    "Zbyt wiele prób logowania, spróbuj ponownie za {1} minut.",
+    "Zbyt wiele żądań logowania, spróbuj ponownie za {1} minut.",
 };
