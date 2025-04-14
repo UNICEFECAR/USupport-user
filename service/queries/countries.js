f24b60d2c28c08e3d36154b35f2d4b7e8105ad44
7Digit-Main <it-support@7digit.io>
2025-04-14 18:58:55 +0300

    Create: route to get all countries with their languages

--- service/queries/countries.js
+++ service/queries/countries.js
@@ -35,3 +35,40 @@ export const getCountryByAlpha2CodeQuery = async ({ country }) =>
     `,
     [country]
   );
+
+export const getCountriesWithLanguagesQuery = async () => {
+  return await getDBPool("masterDb").query(
+    `
+          SELECT 
+              country.country_id,
+              country.name,
+              country.alpha2,
+              country.alpha3,
+              country.is_active,
+              country.min_client_age, 
+              country.max_client_age,
+              currency.currency_id, 
+              currency.name AS currency_name,
+              currency.code, 
+              currency.symbol, 
+              currency.min_price, 
+              country.local_name AS local_name,
+              JSON_AGG(DISTINCT 
+                  JSONB_BUILD_OBJECT(
+                      'language_id', l.language_id,
+                      'name', l.name,
+                      'alpha2', l.alpha2,
+                      'local_name', l.local_name
+                  )
+              ) FILTER (WHERE l.is_active) AS languages
+          FROM country
+          INNER JOIN country_currency_links ON country.country_id = country_currency_links.country_id
+          INNER JOIN currency ON country_currency_links.currency_id = currency.currency_id
+          LEFT JOIN country_language_links cl ON cl.country_id = country.country_id
+          LEFT JOIN language l ON cl.language_id = l.language_id
+          WHERE country.is_active = true
+          GROUP BY country.country_id, currency.currency_id
+          ORDER BY country.name ASC;
+      `
+  );
+};
