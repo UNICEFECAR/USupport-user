58a3f23bbb9ab18fe4685dba7ebaf7c4699ff066
Stefan Dobrev <stefandobrev88@gmail.com>
2025-08-12 16:15:51 +0300

    Add: Armenian translations

--- service/translations/index.js
+++ service/translations/index.js
@@ -1,10 +1,12 @@
-import en from "./en.js";
-import kk from "./kk.js";
-import ru from "./ru.js";
-import pl from "./pl.js";
-import uk from "./uk.js";
+import am from './am.js';
+import en from './en.js';
+import kk from './kk.js';
+import ru from './ru.js';
+import pl from './pl.js';
+import uk from './uk.js';
 
 const translations = {
+  am,
   en,
   kk,
   ru,
@@ -19,12 +21,12 @@ const translations = {
  * @param {Array} params the parameters to be inserted into the translation
  * @returns {string} the translated string
  */
-export const t = (key, language = "en", params = []) => {
+export const t = (key, language = 'en', params = []) => {
   let translation = undefined;
 
   // Make sure the language exists and if not return the default language
   if (!Object.keys(translations).includes(language)) {
-    translation = translations["en"][key];
+    translation = translations['en'][key];
   } else {
     translation = translations[language][key];
   }
@@ -34,7 +36,7 @@ export const t = (key, language = "en", params = []) => {
       translation = translation.replace(`{${index + 1}}`, param);
     });
   } else {
-    translation = translations["en"][key];
+    translation = translations['en'][key];
   }
 
   return translation;
